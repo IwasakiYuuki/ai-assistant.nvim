6588bca38d91e7c766e9fb34066a34ab7892558f
IwasakiYuuki <2445yuuki@gmail.com>
2023-03-06 22:52:18 +0900

    ウィンドウが自動で閉じるように修正

--- rplugin/python3/ai_assistant/__init__.py
+++ rplugin/python3/ai_assistant/__init__.py
@@ -9,6 +9,7 @@ from ai_assistant.request_chatgpt import RequestChatGPT
 class AIAssistant:
     def __init__(self, nvim: Nvim):
        self.nvim = nvim 
+       self.api = nvim.api
        self.input_buf = None
        self.input_win = None
        self.output_buf = None
@@ -16,7 +17,7 @@ class AIAssistant:
 
     @pynvim.command("AIAssistantToggle")
     def toggle_aiassistant(self):
-        if self.input_win and self.output_win:
+        if self._is_wins_open():
             self.close_input_win()
             self.close_output_win()
         else:
@@ -27,35 +28,40 @@ class AIAssistant:
 
     def open_input_win(self):
         self.nvim.command("vsplit")
-        self.input_win = self.nvim.api.get_current_win()
+        self.input_win = self.api.get_current_win()
         if not self.input_buf:
-            self.input_buf = self.nvim.api.create_buf(False, False)
-            self.nvim.api.buf_set_option(self.input_buf, "readonly", False)
-        self.nvim.api.win_set_buf(self.input_win, self.input_buf)
+            self.input_buf = self.api.create_buf(False, False)
+        self.api.win_set_buf(self.input_win, self.input_buf)
+        self.api.set_option_value("buftype", "nofile", { "scope": "local" })
 
     def close_input_win(self):
-        self.nvim.api.win_close(self.input_win, True)
+        self.api.win_close(self.input_win, True)
         self.input_win = None
 
     def open_output_win(self):
         self.nvim.command("split")
-        self.output_win = self.nvim.api.get_current_win()
+        self.output_win = self.api.get_current_win()
         if not self.output_buf:
-            self.output_buf = self.nvim.api.create_buf(False, False)
-            self.nvim.api.buf_set_option(self.output_buf, "readonly", False)
-        self.nvim.api.win_set_buf(self.output_win, self.output_buf)
+            self.output_buf = self.api.create_buf(False, False)
+        self.api.win_set_buf(self.output_win, self.output_buf)
+        self.api.set_option_value("buftype", "nofile", { "scope": "local" })
 
     def close_output_win(self):
-        self.nvim.api.win_close(self.output_win, True)
+        self.api.win_close(self.output_win, True)
         self.output_win = None
+
+    def _is_wins_open(self):
+        input_win = self.api.win_is_valid(self.input_win) if self.input_win else False
+        output_win = self.api.win_is_valid(self.output_win) if self.output_win else False
+        return input_win and output_win
 
     @pynvim.command("AIAssistantCodeCompletion")
     def code_completion(self):
         if not (self.input_win and self.output_win):
-            self.toggle_aiassistant()        
+            self.toggle_aiassistant()
 
-        instruction = self.nvim.api.buf_get_lines(self.input_buf, 0, -1, False)
+        instruction = self.api.buf_get_lines(self.input_buf, 0, -1, False)
         instruction = "\n".join(instruction)
         res = RequestChatGPT.generate_code(instruction)
         res = res.split("\n")
-        self.nvim.api.buf_set_lines(self.output_buf, 0, -1, False, res)
+        self.api.buf_set_lines(self.output_buf, 0, -1, False, res)
